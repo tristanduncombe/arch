455080226d106a6ad7afa74bf0d17216f837d24a
liv <o.ronda@uqconnect.edu.au>
2024-04-03 12:44:30 +1000

    mock data

--- arch/src/pages/Leaderboard.tsx
+++ arch/src/pages/Leaderboard.tsx
@@ -1,5 +1,5 @@
 
-import { Box, Card, CardActionArea, CardContent, Paper, Typography } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, Paper, Stack, Typography } from '@mui/material';
 import React from 'react';
 
 const mockData = [
@@ -7,37 +7,42 @@ const mockData = [
     {name: 'Lakshan Chandrasekara', points: 100},
     {name: 'Jackson Trenerry', points: 0},
     {name: 'Liv Ronda', points: 1000},
+    {name: 'Random Names', points: 345},
+    {name: 'Sana Mccormick', points: 54},
+    {name: 'Yasir Mayo', points: 601},
+    {name: 'Valerie Copeland', points: 736},
+    {name: 'Conrad Ramirez', points: 276},
+    {name: 'Talia Carr', points: 777},
+    {name: 'Lilian Rowe', points: 503},
+    {name: 'Harrison Collier', points: 594},
+    {name: 'Niall Pitts', points: 879},
+    {name: 'Harris Key', points: 654},
 ]
 
 function Leaderboard() {
     return (
-        <Box
-          sx={{
-            flexGrow: 1,
-            backgroundColor: "#c2c2c2",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Paper elevation={3} sx={{maxWidth: "lg", width: "100%", px: 3, py: 3}}>
-              {mockData.sort((a,b) => {
-                  if (a.points > b.points) return -1;
-                  if (a.points < b.points) return 1;
-                  return 0;
-              }).map((d) => (
-                  <Card sx={{ width: '100%', height: '50px', mb: 1 }}>
-                      <CardActionArea
-                          onClick={() => alert('omg')}
-                      >
-                        <CardContent>
-                            {`${d.name} | ${d.points}pts`}
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-              ))}
-          </Paper>
-        </Box>
+      <Paper elevation={3} sx={{maxWidth: "lg", width: "100%", height: '100%', px: 3, py: 3}}>
+          {mockData.sort((a,b) => {
+              if (a.points > b.points) return -1;
+              if (a.points < b.points) return 1;
+              return 0;
+          }).map((d) => (
+              <Card sx={{ width: '100%', height: '50px', mb: 1 }}>
+                  <CardActionArea>
+                    <CardContent>
+                        <Stack direction="row" justifyContent="space-between">
+                            <Typography>
+                                {d.name}
+                            </Typography>
+                            <Typography>
+                                {d.points}pts
+                            </Typography>
+                        </Stack>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+          ))}
+      </Paper>
     )
 }
 export default Leaderboard;
