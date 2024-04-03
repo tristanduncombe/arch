99aa6e37cc57118919f37a6bf681a2bc6cc4f5b2
Tristan Duncombe <tduncombe747@gmail.com>
2024-04-03 10:54:23 +1000

    Delete files

--- arch/src/pages/Home.tsx
+++ arch/src/pages/Home.tsx
@@ -2,30 +2,26 @@ import React, { ReactElement, FC } from "react";
 import { Box, Paper, Typography } from "@mui/material";
 
 const Home: FC<any> = (): ReactElement => {
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: "#c2c2c2",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Paper elevation={3} sx={{maxWidth: "lg", width: "100%", px: 3, py: 3}}>
-        <Typography variant="h2">
-          ReactAttic
-        </Typography>
-        <Typography variant="h6">
-          Like UQAttic, but way worse!
-        </Typography>
-        <Typography sx={{my: 2}}>
-          Our goal is provide answers to tests from the University of Queensland.
-        </Typography>
-      </Paper>
-
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                backgroundColor: "#c2c2c2",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Paper
+                elevation={3}
+                sx={{ maxWidth: "lg", width: "100%", px: 3, py: 3 }}
+            >
+                <Typography variant="h2"></Typography>
+                <Typography variant="h6"></Typography>
+                <Typography sx={{ my: 2 }}></Typography>
+            </Paper>
+        </Box>
+    );
 };
 
 export default Home;
